854773a05317082a65e35739fc26d67a8cb9f8dd
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2019-12-01 12:48:34 -0300

    Inclusão de Title

--- grafico.py
+++ grafico.py
@@ -1,8 +1,13 @@
 # Visualização de dados em Python
 import matplotlib.pyplot as plt
 
-x = [1,2]
-y = [2,3]
+x = [1,2,5]
+y = [2,3,7]
+
+plt.title("Gráfico em python")
+
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
 
 plt.plot(x, y)
 plt.show()
