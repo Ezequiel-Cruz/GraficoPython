a52f76171929809ae276aec598f32fc9ea776d02
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2019-12-04 18:53:00 -0300

    Barras

--- grafico.py
+++ grafico.py
@@ -1,13 +1,16 @@
 # Visualização de dados em Python
 import matplotlib.pyplot as plt
 
-x = [1,2,5]
-y = [2,3,7]
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 7, 1, 0]
 
-plt.title("Gráfico em python")
+titulo = "Gráfico de barras"
+eixox = "Eixo X"
+eixoy = "Eixo Y"
 
-plt.xlabel("Eixo X")
-plt.ylabel("Eixo Y")
+plt.title(titulo)
+plt.xlabel(eixox)
+plt.ylabel(eixoy)
 
-plt.plot(x, y)
+plt.bar(x, y)
 plt.show()
